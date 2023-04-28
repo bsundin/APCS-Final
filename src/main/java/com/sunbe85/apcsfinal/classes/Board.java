37c388fd5974168b12835557dee8cb2b6d843837
sunbe85 <sunbe85@libertycommon.org>
2023-04-28 13:55:52 -0600

    Worked on board square name list

--- src/main/java/com/sunbe85/apcsfinal/classes/Board.java
+++ src/main/java/com/sunbe85/apcsfinal/classes/Board.java
@@ -3,8 +3,15 @@ package com.sunbe85.apcsfinal.classes;
 import com.sunbe85.apcsfinal.interfaces.Renderable;
 import javafx.scene.canvas.Canvas;
 
-public class Board extends GameElement implements Renderable {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Board extends GameElement implements Renderable {
+    private List<BoardSquare> squares = new ArrayList<>();
+    private String[] names;
+    public Board() {
+        names = new String[] {"Go","Mediterranean Avenue", "Community Chest", "Baltic Avenue", "Income Tax", "Reading Railroad", "Oriental Avenue", "Chance", "Vermont", "Connecticut", "Jail", "St. Charles Place", "Electric Company", "States Avenue", "Virginia Avenue", "Pennsylvania Railroad", "St. James Place", "Community Chest", "Tennessee Avenue", "New York Avenue", "Free Parking", "Kentucky Avenue", "Chance", "Indiana Avenue", "Illinois Avenue", "B. & O. Railroad", "Atlantic Avenue", "Ventnor Avenue", "Water Works", "Marvin Gardens", "Go To Jail"};
+    }
     @Override
     public void draw(Canvas cs) {
 
--- src/main/java/com/sunbe85/apcsfinal/classes/Monopoly.java
+++ src/main/java/com/sunbe85/apcsfinal/classes/Monopoly.java
@@ -2,6 +2,6 @@ package com.sunbe85.apcsfinal.classes;
 
 public class Monopoly {
     public Monopoly() {
-
+        Board b = new Board();
     }
 }
